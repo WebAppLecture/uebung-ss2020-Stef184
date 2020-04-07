b789185bf4380751c2ed442d7bf100f2a939fe8f
Stef184 <swisneth@gmx.de>
2020-04-07 14:18:41 +0200

    Versuch_1

--- 02-Calculator/Calculator.js
+++ 02-Calculator/Calculator.js
@@ -4,13 +4,26 @@ export class Calculator {
 
     constructor(numpad, outputCalculation, outputSolution) {
     }
+    window.calc = new Calculator();
+            this.numPad = numPad;
+            this.outputCalc = outputCalc;
+            this.outputSolution = outputSolution;
+            this.setupNumpad()
 
     setupNumPad() { 
+        for(int element : this.numPad.children){
+            element.addEventListener('click', this.onButtonClick.bind(this));
+        }
+
+      /* for (int i = 0; i>=9; i++){
+            index.print(<button>i</button>)
+      }
+        }*/
 
     }
 
     onButtonClick(symbol) {
-    
+        console.log()
     }
 
     print(string) {
