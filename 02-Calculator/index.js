69d16593adb20a3b0857dc19409473b8655ada92
Stef184 <swisneth@gmx.de>
2020-04-07 16:03:56 +0200

    v3

--- 02-Calculator/index.js
+++ 02-Calculator/index.js
@@ -1,7 +1,7 @@
 import { Calculator } from './Calculator.js'
 window.Calculator = Calculator; // fügt die Klasse zum globalen NameSpace hinzu
-let numpad = document.querySelector("#numPad");
-    outputCalc = document.querySelector("#outputCalc");
-    outputSolution = document.querySelector("#outputSolution");
+let numpad = document.querySelector(".numPad");
+    outputCalc = document.querySelector(".outputCalc");
+    outputSolution = document.querySelector(".outputSolution");
     
 window.calc = new Calculator(numpad, outputCalc, outputSolution);
